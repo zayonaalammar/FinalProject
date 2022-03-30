923b62e8aa5a7a5795fcb4a1af4fff136166b64e
zayona <alzain05@icloud.com>
2022-03-31 01:05:08 +0300

    done

---
+++ الزكاة و الصدقات.js
@@ -0,0 +1,10 @@
+function myfun() {
+  var a = document.getElementById("number").value;
+  var b = document.getElementById("numbers").value;
+  document.getElementById("result").innerHTML = a + b;
+}
+function myfun1() {
+  var a = document.getElementById("number").value;
+  var b = document.getElementById("numbers").value;
+  document.getElementById("result").innerHTML = a - b;
+}
